d5ff30bc412be6a860a358802d94243fa21de69f
ssong <yysk_915@naver.com>
2022-03-09 01:22:48 +0900

    [Add] ~1-10

--- week01/src/main/java/com/sparta/week01/prac/Prac.java
+++ week01/src/main/java/com/sparta/week01/prac/Prac.java
@@ -5,29 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Prac {
-    public static int countFruit(String given) {
-        List<String> fruits = new ArrayList<>();
-        fruits.add("수박");
-        fruits.add("메론");
-        fruits.add("수박");
-        fruits.add("딸기");
-        fruits.add("메론");
-        fruits.add("수박");
-        fruits.add("메론");
-        fruits.add("감");
-
-        int count = 0;
-        for (int i = 0; i < fruits.size(); i++) {
-            String fruit = fruits.get(i);
-            if (given == fruit) {
-                count += 1;
-            }
-        }
-        return count;
-    }
-
     public static void main(String[] args) {
-        System.out.println(countFruit("메론"));
+        Tutor tutor = new Tutor();
+        String name = "Tom";
+        int bio = 25;
+        tutor.setBio(bio);
+        tutor.setName(name);
+        System.out.println(tutor.getBio());
+        System.out.println(tutor.getName());
 
     }
+
 }
---
+++ week01/src/main/java/com/sparta/week01/prac/Tutor.java
@@ -0,0 +1,15 @@
+package com.sparta.week01.prac;
+
+public class Tutor {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name ;
+    private int bio;
+
+}
