66d86022c3fd8ea5956f6737e423d81e140181dc
ssong <yysk_915@naver.com>
2022-03-11 22:11:11 +0900

    [Add] ~2-5

--- week02/src/main/java/com/sparta/week02/Week02Application.java
+++ week02/src/main/java/com/sparta/week02/Week02Application.java
@@ -1,7 +1,13 @@
 package com.sparta.week02;
 
+import com.sparta.week02.domain.Course;
+import com.sparta.week02.domain.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Week02Application {
@@ -10,4 +16,22 @@ public class Week02Application {
 		SpringApplication.run(Week02Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(CourseRepository repository) {
+		return (args) -> {
+			// 테이블 생성
+			Course course1 = new Course("웹개발의 봄","남병관");
+			// 테이블에 저장
+			repository.save(course1);
+			// 테이블 조회
+			List<Course> courseList =  repository.findAll();
+			for(int i =0;i<courseList.size();i++){
+				Course c = courseList.get(i);
+				System.out.println(c.getTitle());
+				System.out.println(c.getTutor());
+			}
+			// id 로 찾고 싶을 때: repository.findById();
+
+		};
+	}
 }
