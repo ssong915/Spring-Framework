1c4bf60716753671ba757cde32cf4128e84c1389
ssong <yysk_915@naver.com>
2022-03-11 23:46:26 +0900

    [Add] 2-7

--- week02/src/main/java/com/sparta/week02/Week02Application.java
+++ week02/src/main/java/com/sparta/week02/Week02Application.java
@@ -1,5 +1,6 @@
 package com.sparta.week02;
 
+import com.sparta.week02.Service.CourseService;
 import com.sparta.week02.domain.Course;
 import com.sparta.week02.domain.CourseRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -14,26 +15,35 @@ import java.util.List;
 @SpringBootApplication
 public class Week02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Week02Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Week02Application.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner demo(CourseRepository repository) {
-		return (args) -> {
-			// 테이블 생성
-			Course course1 = new Course("웹개발의 봄","남병관");
-			// 테이블에 저장
-			repository.save(course1);
-			// 테이블 조회
-			List<Course> courseList =  repository.findAll();
-			for(int i =0;i<courseList.size();i++){
-				Course c = courseList.get(i);
-				System.out.println(c.getTitle());
-				System.out.println(c.getTutor());
-			}
-			// id 로 찾고 싶을 때: repository.findById();
+    @Bean
+    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
+        return (args) -> {
+            courseRepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
 
-		};
-	}
+            System.out.println("데이터 인쇄");
+            List<Course> courseList = courseRepository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+
+            Course new_course = new Course("웹개발의 봄, Spring", "임민영");
+            courseService.update(1L, new_course);
+            courseList = courseRepository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+
+            courseRepository.deleteAll();
+        };
+    }
 }
