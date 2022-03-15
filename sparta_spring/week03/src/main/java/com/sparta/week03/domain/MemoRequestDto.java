b84baa7f46db9cc05e13909af4fc142bfde7b05f
ssong <yysk_915@naver.com>
2022-03-15 23:56:34 +0900

    [Add] 3-3

---
+++ sparta_spring/week03/src/main/java/com/sparta/week03/domain/MemoRequestDto.java
@@ -0,0 +1,13 @@
+package com.sparta.week03.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class MemoRequestDto {
+    private final String username;
+    private final String contents;
+}
---
+++ sparta_spring/week03/src/main/java/com/sparta/week03/domain/Timestamped.java
@@ -0,0 +1,20 @@
+package com.sparta.week03.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass // Entity가 자동으로 컬럼으로 인식합니다.
+@EntityListeners(AuditingEntityListener.class) // 생성/변경 시간을 자동으로 업데이트합니다.
+public abstract class Timestamped { // abstract: 상속을 받아야만 할 수 있는 것
+
+    @CreatedDate //생성시간
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate //마지막수정시간
+    private LocalDateTime modifiedAt;
+}
