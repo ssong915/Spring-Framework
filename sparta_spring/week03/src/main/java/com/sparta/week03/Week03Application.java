e5b5c67b2ea77891104b391cff62cdfa7b815d92
ssong <yysk_915@naver.com>
2022-03-22 18:00:03 +0900

    [Add] post,put,delete

--- sparta_spring/week03/src/main/java/com/sparta/week03/Week03Application.java
+++ sparta_spring/week03/src/main/java/com/sparta/week03/Week03Application.java
@@ -2,7 +2,10 @@ package com.sparta.week03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Week03Application {
 
--- sparta_spring/week03/src/main/java/com/sparta/week03/domain/Timestamped.java
+++ sparta_spring/week03/src/main/java/com/sparta/week03/domain/Timestamped.java
@@ -1,5 +1,6 @@
 package com.sparta.week03.domain;
 
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -8,6 +9,8 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+/**세개 다 필수!!**/
+@Getter
 @MappedSuperclass // Entity가 자동으로 컬럼으로 인식합니다.
 @EntityListeners(AuditingEntityListener.class) // 생성/변경 시간을 자동으로 업데이트합니다.
 public abstract class Timestamped { // abstract: 상속을 받아야만 할 수 있는 것
@@ -17,4 +20,6 @@ public abstract class Timestamped { // abstract: 상속을 받아야만 할 수 
 
     @LastModifiedDate //마지막수정시간
     private LocalDateTime modifiedAt;
+
+
 }
