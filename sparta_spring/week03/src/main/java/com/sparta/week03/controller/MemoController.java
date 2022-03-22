f3d3477ceacd3087598b8663c2758a0ffe259a8b
ssong <yysk_915@naver.com>
2022-03-22 18:37:53 +0900

    [Add] 24시간 이내 메모만 조회

--- sparta_spring/week03/src/main/java/com/sparta/week03/controller/MemoController.java
+++ sparta_spring/week03/src/main/java/com/sparta/week03/controller/MemoController.java
@@ -7,6 +7,7 @@ import com.sparta.week03.service.MemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -17,16 +18,20 @@ public class MemoController {
     private final MemoRepository memoRepository;
     private final MemoService memoService;
 
+
+    @GetMapping("/api/memos")
+    public List<Memo> getMemos() {
+        LocalDateTime start = LocalDateTime.now().minusDays(1);
+        LocalDateTime end = LocalDateTime.now();
+        return memoRepository.findAllByModifiedAtBetweenOrderByModifiedAtDesc(start, end);
+    }
+    }
+
     @PostMapping("/api/memos")
     public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
         // @RequestBody : 요청이 오면, Body에 쓰여진 애를 Dto에 넣어줘! 라는 annotation
         Memo memo = new Memo(requestDto);
         return memoRepository.save(memo);
-    }
-
-    @GetMapping("/api/memos")
-    public List<Memo> getMemos() {
-        return memoRepository.findAllByOrderByModifiedAtDesc();
     }
 
     @DeleteMapping("/api/memos/{id}")
--- sparta_spring/week03/src/main/java/com/sparta/week03/domain/MemoRepository.java
+++ sparta_spring/week03/src/main/java/com/sparta/week03/domain/MemoRepository.java
@@ -2,8 +2,9 @@ package com.sparta.week03.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> { //JpaRepository 상속, Memo라는 녀석의 id가 Long인 녀석
-    List<Memo> findAllByOrderByModifiedAtDesc(); // 생성시간 최신순을 정렬해주셈
+    List<Memo> findAllByModifiedAtBetweenOrderByModifiedAtDesc(LocalDateTime start, LocalDateTime end); // 생성시간 최신순을 정렬해주셈
 }
