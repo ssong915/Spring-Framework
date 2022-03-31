ff73e66c29a11f0406cf22308be16e93caae6e6b
ssong <yysk_915@naver.com>
2022-03-31 15:47:46 +0900

    [Add] 4-4

---
+++ sparta_spring/week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -0,0 +1,28 @@
+package com.sparta.week04.utils;
+
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+public class NaverShopSearch {
+    public String search() {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-Id", "PdjnhQLp9GtX8J7Tn1CA");
+        headers.add("X-Naver-Client-Secret", "B25hVd7toy");
+        String body = "";
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=iphone", HttpMethod.GET, requestEntity, String.class);
+        HttpStatus httpStatus = responseEntity.getStatusCode();
+        int status = httpStatus.value();
+        String response = responseEntity.getBody();
+        System.out.println("Response status: " + status);
+        System.out.println(response);
+        return response;
+    }
+
+    public static void main(String[] args) {
+        NaverShopSearch naverShopSearch = new NaverShopSearch();
+        naverShopSearch.search();
+    }
+}
