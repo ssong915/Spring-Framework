0364a5cfcc133a0445bd9bfc39c3f0e42ca0eff8
songkyung <yysk_915@naver.com>
2022-04-05 02:19:13 +0900

    [Add] 4-8

--- sparta_spring/week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
+++ sparta_spring/week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -1,10 +1,16 @@
 package com.sparta.week04.utils;
 
+import com.sparta.week04.models.ItemDto;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NaverShopSearch {
-    public String search() {
+    public String search(String query) {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Naver-Client-Id", "PdjnhQLp9GtX8J7Tn1CA");
@@ -12,7 +18,7 @@ public class NaverShopSearch {
         String body = "";
 
         HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=iphone", HttpMethod.GET, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query="+query, HttpMethod.GET, requestEntity, String.class);
         HttpStatus httpStatus = responseEntity.getStatusCode();
         int status = httpStatus.value();
         String response = responseEntity.getBody();
@@ -21,8 +27,24 @@ public class NaverShopSearch {
         return response;
     }
 
-    public static void main(String[] args) {
-        NaverShopSearch naverShopSearch = new NaverShopSearch();
-        naverShopSearch.search();
+    public List<ItemDto> fromJSONtoItems(String result){
+        // 문자열 정보를 JSONObject로 바꾸기
+        JSONObject rjson = new JSONObject(result);
+        System.out.println(rjson);
+        // JSONObject에서 items 배열 꺼내기
+        // JSON 배열이기 때문에 보통 배열이랑 다르게 활용해야한다.
+        JSONArray items = rjson.getJSONArray("items");
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        for (int i=0; i<items.length(); i++) {
+            JSONObject itemJson = (JSONObject) items.get(i);
+            ItemDto itemDto = new ItemDto(itemJson);
+            itemDtoList.add(itemDto);
+//            // item 중에서 필요한 항목만 꺼내기
+//            String title = itemJson.getString("title");
+//            String image = itemJson.getString("image");
+//            String link = itemJson.getString("link");
+//            int lprice = itemJson.getInt("lprice");
+        }
+        return itemDtoList;
     }
 }
