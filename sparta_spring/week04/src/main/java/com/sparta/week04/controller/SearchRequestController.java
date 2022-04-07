3e8ff096ae9c76cbb3c5668ddae585cbbac00b8c
songkyung <yysk_915@naver.com>
2022-04-07 22:56:38 +0900

    [Add] 4-10

---
+++ sparta_spring/week04/src/main/java/com/sparta/week04/controller/SearchRequestController.java
@@ -0,0 +1,24 @@
+package com.sparta.week04.controller;
+
+import com.sparta.week04.models.ItemDto;
+import com.sparta.week04.utils.NaverShopSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor // final 로 선언된 클래스를 자동으로 생성합니다.
+@RestController // JSON으로 응답함을 선언합니다.
+public class SearchRequestController {
+
+    private final NaverShopSearch naverShopSearch;
+
+    @GetMapping("/api/search")
+    public List<ItemDto> getItems(@RequestParam String query) {
+        // ? 뒤에 오는 넘을 쓰고 싶다면 @RequestParam 필수
+        String resultString = naverShopSearch.search(query);
+        return naverShopSearch.fromJSONtoItems(resultString);
+    }
+}
